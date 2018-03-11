66ab9b961acbf66ebe1cc11c11591ee97e32c3ed
erick <834424581@qq.com>
2018-03-11 14:21:40 +0800

    商品列表页基础建设json返回数据

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 # from django.contrib import admin
 import xadmin
+from goods.views_base import GoodsListView
 
 from MxShop.settings import MEDIA_ROOT
 from django.views.static import serve
@@ -23,4 +24,5 @@ from django.views.static import serve
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^media/(?P<path>.*)$',serve,{'document_root':MEDIA_ROOT}),
+    url(r'goods/$',GoodsListView.as_view(),name='goods-list')
 ]
