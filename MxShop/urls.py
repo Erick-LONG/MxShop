0cd1f506d96072a7d78b5e7219f7f83ed5153761
erick <834424581@qq.com>
2018-03-12 21:53:07 +0800

    配置jwt登录验证，原理：客户端携带用户名密码post到jwt，服务器返回加密token，客户端设置header 的authencation加入JWT+token get请求数据

--- MxShop/urls.py
+++ MxShop/urls.py
@@ -22,6 +22,7 @@ from django.views.static import serve
 from rest_framework.documentation import include_docs_urls
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+from rest_framework_jwt.views import obtain_jwt_token
 
 router = DefaultRouter()
 #配置goods URL
@@ -34,5 +35,10 @@ urlpatterns = [
     url(r'^',include(router.urls)),
     url(r'docs/',include_docs_urls(title='慕学生鲜')),
     url(r'^api-auth/', include('rest_framework.urls')),
-    url(r'^api-token-auth/', views.obtain_auth_token)
+
+    #drf自带的token认证模式
+    url(r'^api-token-auth/', views.obtain_auth_token),
+
+    #jwt的认证接口
+    url(r'^jwt-auth/', obtain_jwt_token)
 ]
