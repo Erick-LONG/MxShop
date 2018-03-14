ef79087abf378f010311f97f6de620c80ec43aa5
erick <834424581@qq.com>
2018-03-14 22:43:42 +0800

    商品详情页数据展示

--- apps/goods/serializers.py
+++ apps/goods/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Goods,GoodsCategory
+from .models import Goods,GoodsCategory,GoodsImage
 
 
 class CategorySerializer3(serializers.ModelSerializer):
@@ -24,8 +24,15 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class GoodsImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoodsImage
+        fields = ("image",)
+
+
 class GoodsSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
+    images = GoodsImageSerializer(many=True)
 
     class Meta:
         model = Goods
