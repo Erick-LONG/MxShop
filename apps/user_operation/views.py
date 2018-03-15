376f390bfe23f2bcedbeacb5c885d4983654c5a2
erick <834424581@qq.com>
2018-03-15 22:31:28 +0800

    删除添加权限验证

--- apps/user_operation/views.py
+++ apps/user_operation/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import mixins
+from rest_framework.permissions import IsAuthenticated
 from .serializer import UserFavSerializer
 from .models import UserFav
+from utils.permissions import IsOwnerOrReadOnly
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
+from rest_framework.authentication import SessionAuthentication
 
 
 class UserFavViewSet(mixins.CreateModelMixin,mixins.ListModelMixin,mixins.DestroyModelMixin,viewsets.GenericViewSet):
     '''
     用户收藏功能
     '''
-    queryset = UserFav.objects.all()
+    permission_classes = (IsAuthenticated,IsOwnerOrReadOnly)
     serializer_class = UserFavSerializer
+    authentication_classes = (JSONWebTokenAuthentication,SessionAuthentication)
+
+    def get_queryset(self):
+        return UserFav.objects.filter(user = self.request.user)
